d8f865fb02fbdf833ba39c6a717025c25b7861c9
Simon Colmer <simon_colmer@hotmail.com>
2016-11-29 17:53:36 +0000

    cleanup

--- src/RaisingIt.RecruitmentTest.Web/Models/CampaignViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using RaisingIt.RecruitmentTest.Domain.Campaigns;
-
-namespace RaisingIt.RecruitmentTest.Web.Models
-{
-    public class CampaignViewModel
-    {
-        public Campaign[] Campaigns { get; set; }
-    }
-}
